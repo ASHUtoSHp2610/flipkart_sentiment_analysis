8b439f03483610cdea33e8ba49562ba86cb2709c
Ashutosh Pawar <ashrampawar765@gmail.com>
2024-03-14 12:18:10 +0530

    second commit

--- app.py
+++ app.py
@@ -1,48 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+
 import joblib
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace 'your_secret_key' with your own secret key
 
-# Sample user data (in a real application, this would be stored securely)
-users = {
-    "user1": {"email": "user1@example.com", "password": "password1"},
-    "user2": {"email": "user2@example.com", "password": "password2"}
-}
-
-# Load the trained model
 best_rf_model = joblib.load('best_rf_model.pkl')
 
-# Load the TF-IDF vectorizer
 tfidf_vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 
 @app.route('/')
 def index():
     error = request.args.get('error')
-    return render_template('login.html', error=error)
+    return render_template('index.html', error=error)
 
 @app.route('/portal')
 def portal():
-    if 'username' in session:
         return render_template('portal.html')
-    else:
-        return redirect(url_for('index', error=True))
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
-
-    # Check if the username and password match a user in the database
-    if username in users and users[username]['password'] == password:
-        # Store the username in the session
-        session['username'] = username
-        # Redirect to the portal page upon successful login
-        return redirect(url_for('portal'))
-    else:
-        # Redirect back to the login page with an error message
-        return redirect(url_for('index', error=True))
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
@@ -50,10 +28,15 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+@app.route('/signup')
+def sign():
+    return render_template('sign.html')
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
     if 'username' in session:
         review = request.form['review']
+        rating = int(request.form['rating'])  # Convert rating to integer
         # Perform text preprocessing (e.g., tokenization, stop word removal)
         processed_review = preprocess(review)
         # Vectorize the review using the TF-IDF vectorizer
@@ -61,9 +44,10 @@ def analyze():
         # Predict the sentiment of the review using the trained model
         sentiment_prediction = best_rf_model.predict(vectorized_review)
         # Render the template with the prediction result
-        return render_template('analysis_result.html', review=review, sentiment=sentiment_prediction[0])
+        return render_template('analysis_result.html', review=review, rating=rating, sentiment=sentiment_prediction[0])
     else:
         return redirect(url_for('index', error=True))
+
 
 def preprocess(text):
     # Implement your text preprocessing steps here
